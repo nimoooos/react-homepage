e37d79c78d212d822e05d9766bdb9820cf0130d6
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-04-04 21:44:40 -1000

    Update App.js
    reorganized page, documented import statements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+//Use for AppRouter
 import { Routes, Route } from "react-router-dom"
+
+//react-bootstrap import
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Stack from 'react-bootstrap/Stack';
-import Badge from 'react-bootstrap/Badge';
+
+//Component import
+import Navi from './Subcomponents/Navi';
+import CallingCard from "./Subcomponents/CallingCard";
+import Todo from './Components/Todo';
+import Cats from './Components/Cats';
 
 
-import Signpost from './Components/Signpost';
-import Navi from './Components/Navi';
 import './App.css';
+
 
 
 function App() {
@@ -16,63 +21,15 @@ function App() {
     <>
       <Navi />
       <Container>
-        <Row><h1>Welcome to Soomin's Website</h1></Row>
-        <Row>
-          <Stack gap={3}>
-            <h3>Coming Soon (To Do list)</h3>
-            <h5>Add in customizable props to Signpost <Badge bg="danger">1</Badge></h5>
-            <h5>Set up router <Badge bg="danger">1</Badge></h5>
-            <h5>Make something fun and interesting (good for my mental health) <Badge bg="warning">2</Badge></h5>
-            <h5>Do something else with navbar <Badge bg="info">3</Badge></h5>
-            <h5>Set up resume <Badge bg="info">3</Badge></h5>
-          </Stack>
-        </Row>
-        <Row><h1>Cats</h1></Row>
-
-        <Row as="div" className='my-5'>
-          <Col>
-            <Signpost />
-          </Col>
-          <Col>
-            <Signpost />
-          </Col>
-          <Col>
-            <Signpost />
-          </Col>
-        </Row>
-
-        <Row as="div" className='my-5'>
-          <Col>
-            <Signpost />
-          </Col>
-          <Col>
-            <Signpost />
-          </Col>
-          <Col>
-            <Signpost />
-          </Col>
-        </Row>
-
-        <Row as="div" className='my-5'>
-          <Col>
-            <Signpost />
-          </Col>
-          <Col>
-            <Signpost />
-          </Col>
-          <Col>
-            <Signpost />
-          </Col>
-        </Row>
-
         <div className="AppRouter">
           <Routes>
             <Route path="/" element={<Todo />} />
           </Routes>
         </div>
 
+        <Cats/>
+        <CallingCard id="tortuga" itemName="tortuga" imgSrc="https://d2c79xe1p61csc.cloudfront.net/frames/mf_tortuga.png" description="This is not a cat" />
       </Container>
-
     </>
   );
 }
