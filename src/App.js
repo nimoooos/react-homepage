991a5ed26639c7b27832adde715037f038924b61
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-04-04 21:42:08 -1000

    Router Implementation
    using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -63,6 +64,12 @@ function App() {
             <Signpost />
           </Col>
         </Row>
+
+        <div className="AppRouter">
+          <Routes>
+            <Route path="/" element={<Todo />} />
+          </Routes>
+        </div>
 
       </Container>
 
