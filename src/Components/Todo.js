8ed139eb1b587a3054174357343f35a22d149e36
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-04-07 07:16:19 -1000

    minor changes

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,18 +3,20 @@ import Badge from 'react-bootstrap/Badge';
 
 export default function Todo() {
   let todolist = [];
-  todolist.push({ task: "Make something fun and interesting (good for my mental health)", priority: 1 });
-  todolist.push({ task: "Do something else with navbar", priority: 1 });
-  todolist.push({ task: "Change website title and icon and stuff", priority: 1 });
-  todolist.push({ task: "Create assets for calling cards", priority: 1 });
-  todolist.push({ task: "Set up resume", priority: 2 });
+  todolist.push({ task: "Make something fun and interesting (good for my mental health)", priority: 2 });
+  todolist.push({ task: "Do something else with navbar", priority: 2 });
+  todolist.push({ task: "Change website title and icon and stuff", priority: 3 });
+  todolist.push({ task: "Create assets for calling cards", priority: 3 });
+  todolist.push({ task: "Set up resume", priority: 4 });
   todolist.push({ task: "Celebrate with rice wine", priority: 4 });
   todolist.push({ task: "Host it on Cloudflare", priority: 2 });
+  todolist.push({ task: "Rebuild app with create-cloudflare CLI", priority: 1 });
+  todolist.push({ task: "Seriously, if I don't do this first, I WILL regret it", priority: 1 });
+  todolist.push({ task: "Make an icon", priority: 3 });
   todolist.push({ task: "Automate hosting", priority: 3 });
 
+
   todolist.sort((a,b)=>a.priority-b.priority);
-
-
 
   return (
     <Stack id="todolist" gap={3}>
