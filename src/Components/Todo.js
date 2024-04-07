d5ca871b4c2b4a1e97363212c996d0520d2c0f0d
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-04-07 08:32:58 -1000

    Update Todo.js

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -5,11 +5,10 @@ export default function Todo() {
   let todolist = [];
   todolist.push({ task: "Make something fun and interesting (good for my mental health)", priority: 2 });
   todolist.push({ task: "Do something else with navbar", priority: 2 });
-  todolist.push({ task: "Change website title and icon and stuff", priority: 3 });
-  todolist.push({ task: "Create assets for calling cards", priority: 3 });
+  todolist.push({ task: "Change website title and icon and stuff", priority: 1 });
   todolist.push({ task: "Set up resume", priority: 4 });
-  todolist.push({ task: "Celebrate with rice wine", priority: 4 });
-  todolist.push({ task: "Make an icon", priority: 3 });
+  todolist.push({ task: "Celebrate with rice wine", priority: 3 });
+  todolist.push({ task: "Make an icon", priority: 2 });
 
 
   todolist.sort((a,b)=>a.priority-b.priority);
