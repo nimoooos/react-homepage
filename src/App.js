38ae88372dcdbb7d28657a299f865704860c6335
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-04-07 08:27:45 -1000

    Links added to existing buttons

--- src/App.js
+++ src/App.js
@@ -31,6 +31,8 @@ function App() {
         <Row>
           <CallingCard id="tortuga" itemName="IPS-N Tortuga" imgSrc="https://d2c79xe1p61csc.cloudfront.net/frames/mf_tortuga.png" description="This is not a cat" />
           <CallingCard id="blackbeard" itemName="IPS-N Blackbeard" imgSrc="https://d2c79xe1p61csc.cloudfront.net/frames/mf_blackbeard.png" description="This is maybe a cat?" />
+          <CallingCard id="tortuga" url="https://massif-press.itch.io/corebook-pdf-free" itemName="IPS-N Tortuga" imgSrc="https://d2c79xe1p61csc.cloudfront.net/frames/mf_tortuga.png" description="This is not a cat" />
+          <CallingCard id="blackbeard" url="https://massif-press.itch.io/corebook-pdf-free" itemName="IPS-N Blackbeard" imgSrc="https://d2c79xe1p61csc.cloudfront.net/frames/mf_blackbeard.png" description="This is maybe a cat?" />
         </Row>
       </Container>
     </>
