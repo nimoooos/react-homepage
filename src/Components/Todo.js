c0b3809d08e367777ba6a1794da0fb600ae00cfe
soomin.song3.mil <d.song.149@gmail.com>
2024-04-08 15:36:02 -1000

    Update Todo.js
    Todo list organized as a list

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,4 +1,4 @@
-import Stack from 'react-bootstrap/Stack';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Badge from 'react-bootstrap/Badge';
 
 export default function Todo() {
@@ -10,21 +10,21 @@ export default function Todo() {
   todolist.push({ task: "Celebrate with rice wine", priority: 3 });
   todolist.push({ task: "Make an icon", priority: 2 });
 
-
-  todolist.sort((a,b)=>a.priority-b.priority);
+  todolist.sort((a, b) => a.priority - b.priority);
 
   return (
-    <Stack id="todolist" gap={3}>
+    <ListGroup id="todolist" >
       <h3>Coming Soon (To Do list)</h3>
-      {todolist.map(x =>
-        <h5>
-          {x.task} {
-            x.priority === 1 ? <Badge bg="danger">1</Badge>
-              : x.priority === 2 ? <Badge bg="warning">2</Badge>
-                : x.priority === 3 ? <Badge bg="info">3</Badge>
-                  : <Badge bg="success">4</Badge>}
-        </h5>)}
-
-    </Stack>
+      
+        {todolist.map(x =>
+          <ListGroup.Item>
+            {x.task} {
+              x.priority === 1 ? <Badge bg="danger">1</Badge>
+                : x.priority === 2 ? <Badge bg="warning">2</Badge>
+                  : x.priority === 3 ? <Badge bg="info">3</Badge>
+                    : <Badge bg="success">4</Badge>}
+          </ListGroup.Item>)}
+      
+    </ListGroup>
   )
 }
