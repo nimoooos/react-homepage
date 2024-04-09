2f444d79e7aaa6caa08ff56437bd599efe82c19d
soomin.song3.mil <d.song.149@gmail.com>
2024-04-08 15:35:49 -1000

    Update CallingCard.js
    Card properly links without appearing as a link

--- src/Subcomponents/CallingCard.js
+++ src/Subcomponents/CallingCard.js
@@ -11,17 +11,17 @@ export default function CallingCard(props) {
   */
   return (
     <>
-    <Link to={props.url} style={{"text-decoration":"none"}}>
       <Card id={props.id} className="col-md-4 p-0 m-3" style={{ width: "300px" }}>
-        <Card.Img variant="top" src={props.imgSrc} />
-        <Card.Body>
-          <Card.Title className="text-center">{props.itemName}</Card.Title>
-          <Card.Text>
-            {props.description}
-          </Card.Text>
-        </Card.Body>
+        <Link to={props.url} className="text-reset text-decoration-none">
+          <Card.Img variant="top" src={props.imgSrc} />
+          <Card.Body >
+            <Card.Title className="text-center">{props.itemName}</Card.Title>
+            <Card.Text>
+              {props.description}
+            </Card.Text>
+          </Card.Body>
+        </Link>
       </Card>
-      </Link>
     </>
   );
 }
