339d8c2349120b591ac9077b29407f7f47e1a7d9
soomin.song3.mil <d.song.149@gmail.com>
2024-04-09 14:03:59 -1000

    Update Dogs.js
    this still doesn't work. need to figure it out

--- src/Components/Dogs.js
+++ src/Components/Dogs.js
@@ -4,10 +4,12 @@ import AxiosAPI from "../Subcomponents/AxiosAPI"
 import React, { useEffect, useState } from "react";
 
 export default function Dogs() {
-    // const [loaded, setLoaded] = useState(null);
-    // let dog = [];
-    // AxiosAPI({"url": "https://dog.ceo/api/breeds/image/random"})
+    const [dogs, setDogs] = useState([]);
 
+    useEffect(()=>{
+        setDogs(AxiosAPI('https://dog.ceo/api/breeds/image/random')
+        .then(console.log(dogs)));
+    },[])
     
 
     return (
@@ -15,6 +17,7 @@ export default function Dogs() {
             <h3>Dogs</h3>
             <Row>
                 <CallingCard itemName="Dog 1" url="https://dog.ceo/dog-api/" imgSrc="https://images.dog.ceo/breeds/terrier-silky/n02097658_7785.jpg" description="This is a dog" />
+                <CallingCard itemName="Dog 2" url="https://dog.ceo/dog-api/" imgSrc={dogs.message} description="Dog 2" />
                 {/* <img src={AxiosAPI({"url": "https://dog.ceo/api/breeds/image/random"}).then(x=>x.data.message)}></img> */}
             </Row>
         </div>
