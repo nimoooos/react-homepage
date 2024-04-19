88dcc8d5154a6390af9401d25f8ad17755ae1511
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-04-18 16:19:23 -1000

    Update DiscordArbitrationRoute.js

--- src/Components/DiscordArbitrationRoute.js
+++ src/Components/DiscordArbitrationRoute.js
@@ -15,10 +15,14 @@ export default function DiscordArbitrationRoute() {
                 <blockquote className="blockquote"><b>Other remedies.</b> ARBITRATION MEANS THAT YOU WAIVE YOUR RIGHT TO A JURY TRIAL.</blockquote>
             </Container>
             <p>... you'll see that quote.</p>
-            <p>You can opt out, but you have to send an email. Here's a link that provides a template:</p>
+            <p>You can opt out, but you have to send an email. Here's a link that provides a quick template:</p>
             <Button variant="danger" target="_blank" href="mailto:arbitration-opt-out@discord.com?subject=I%20decline%20the%20agreement%20to%20arbitrate.&body=I%20decline%20the%20agreement%20to%20arbitrate.">
                 Decline Now
             </Button>
+            <p>Note: I'm not a lawyer, and I urge you to seek more information on what to do. It's possible that this email isn't enough. Please let me know <a href="https://github.com/nimoooos/react-homepage/issues" target="_blank">here</a>.</p>
+
+            <h2>Discord isn't the only one!</h2>
+            <p>Watch out for other companies doing the same thing. This is known as Arbitration Clause.</p>
         </>
     )
 }
