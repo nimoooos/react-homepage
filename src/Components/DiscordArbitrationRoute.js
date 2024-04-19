4e19836d5bf61ee5127228e89f702dc8bd35f79d
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-04-18 16:25:51 -1000

    fixing noreferer
    Using target="_blank" without rel="noreferrer" (which implies rel="noopener") is a security risk in older browsers: see https://mathiasbynens.github.io/rel-noopener/#recommendation

--- src/Components/DiscordArbitrationRoute.js
+++ src/Components/DiscordArbitrationRoute.js
@@ -5,7 +5,7 @@ export default function DiscordArbitrationRoute() {
         <>
             <h1>Discord Terms of Service</h1>
             <h2>...automatically says you agree to waive the right to a trial.</h2>
-            <Button target="_blank" href="https://discord.com/terms#16" variant="primary">See Discord TOS in new tab</Button>
+            <Button rel="noreferrer" target="_blank" href="https://discord.com/terms#16" variant="primary">See Discord TOS in new tab</Button>
             <br/>
             <br/>
             <br/>
@@ -16,10 +16,10 @@ export default function DiscordArbitrationRoute() {
             </Container>
             <p>... you'll see that quote.</p>
             <p>You can opt out, but you have to send an email. Here's a link that provides a quick template:</p>
-            <Button variant="danger" target="_blank" href="mailto:arbitration-opt-out@discord.com?subject=I%20decline%20the%20agreement%20to%20arbitrate.&body=I%20decline%20the%20agreement%20to%20arbitrate.">
+            <Button variant="danger" rel="noreferrer" target="_blank" href="mailto:arbitration-opt-out@discord.com?subject=I%20decline%20the%20agreement%20to%20arbitrate.&body=I%20decline%20the%20agreement%20to%20arbitrate.">
                 Decline Now
             </Button>
-            <p>Note: I'm not a lawyer, and I urge you to seek more information on what to do. It's possible that this email isn't enough. Please let me know <a href="https://github.com/nimoooos/react-homepage/issues" target="_blank">here</a>.</p>
+            <p>Note: I'm not a lawyer, and I urge you to seek more information on what to do. It's possible that this email isn't enough. Please let me know <a rel="noreferrer" href="https://github.com/nimoooos/react-homepage/issues" target="_blank">here</a>.</p>
 
             <h2>Discord isn't the only one!</h2>
             <p>Watch out for other companies doing the same thing. This is known as Arbitration Clause.</p>
