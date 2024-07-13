b57bbd85bc18d29a92eb18310b915293d849bbcd
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2024-07-12 17:04:31 -1000

    export UI

--- src/Components/Ttrpg/Beacon.js
+++ src/Components/Ttrpg/Beacon.js
@@ -31,8 +31,20 @@ export default function Beacon() {
         } else {
             console.log("Unknown job selected.")
         }
+    }
+
+    function printCharacter() {
 
     }
+    
+    function exportCharacter() {
+
+    }
+
+    function importCharacter() {
+
+    }
+    
 
     return (
         <>
@@ -45,28 +57,36 @@ export default function Beacon() {
                         <Form.Control type="text" size="sm" id="characterPronouns" placeholder={charMoniker.pronouns} onChange={pronounChangeHandler} />
                         <Form.Control type="text" size="sm" id="characterTitle" placeholder={charMoniker.title} onChange={titleChangeHandler} />
                     </div>
-
-                    <h4>Level {charStat.level} {charAncestry.name} {charJob.name}</h4>
-                    <InputGroup className='p-1'>
-                        <Form.Select aria-label='Ancestry' onChange={ancestryChangeHandler}>
-                            <option value={null}>Select Ancestry</option>
-                            {Object.keys(ancestriesObject).map((ancestry) => {
-                                return <option value={ancestry}>{ancestry}</option>
-                            })}
-                        </Form.Select>
-                        {/* <Form.Control type="text" placeholder={charAncestry.name} /> */}
-                        {/* </InputGroup>
-                    <InputGroup className='p-1'> */}
-                        <Form.Select aria-label='Job' onChange={jobChangeHandler}>
-                            <option value={null}>Select a Job</option>
-                            {Object.keys(jobsObject).map((job) => {
-                                return <option value={job}>{job}</option>
-                            })}
-                        </Form.Select>
-                        {/* <Form.Control type="text" placeholder={charJob.name}/> */}
-                    </InputGroup>
                 </Form.Group>
 
+                <h4>Level {charStat.level} {charAncestry.name} {charJob.name}</h4>
+                <InputGroup className='p-1'>
+                    <Form.Select id='AncestrySelect' onChange={ancestryChangeHandler}>
+                        <option value={null}>Select Ancestry</option>
+                        {Object.keys(ancestriesObject).map((ancestry) => {
+                            return <option value={ancestry}>{ancestry}</option>
+                        })}
+                    </Form.Select>
+                    <Form.Select >
+                        <option value={null}>Select Ancestry Trait</option>
+                        {charAncestry.traits.map((trait) => {
+                            return <option value={trait}>{trait}</option>
+                        })}
+
+                    </Form.Select>
+                </InputGroup>
+                <InputGroup className='p-1'>
+                    <Form.Select id="JobSelect" onChange={jobChangeHandler}>
+                        <option value={null}>Select a Job</option>
+                        {Object.keys(jobsObject).map((job) => {
+                            return <option value={job}>{job}</option>
+                        })}
+                    </Form.Select>
+                </InputGroup>
+                <Button className='mx-1' onClick={printCharacter}>Export to Markdown</Button>
+                <Button className='mx-1' onClick={exportCharacter}>Export to JSON</Button>
+                <Button className='mx-1' onClick={importCharacter}>Import from JSON</Button>
+                <Form.Control className='my-1' as="textarea" rows={10}/>
             </Form>
         </>
     )
